cc7e25337fc2b22c761625947951f548538e7a12
Domas <domas.jablonskis@gmail.com>
2020-10-29 20:01:55 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/domas/App.java
@@ -0,0 +1,10 @@
+package se.lexicon.domas;
+
+public class App 
+{
+    public static void main( String[] args ) {
+        double number1 = 1.1;
+        double number2 = 9.9;
+        double result = (number1 + number2);
+        System.out.println(result);}
+}
